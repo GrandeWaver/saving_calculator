830be577916ebd4edb64d22f1fc835c7ba9f29ba
Waver <kacpermachay@gmail.com>
2023-01-16 18:23:05 +0100

    update

--- sp500.py
+++ sp500.py
@@ -7,7 +7,7 @@ from best import best
 
 st.set_page_config(layout="wide")
 
-st.title("Informacje")
+st.title("Oszczędzanie")
 
 col1,col2, col3 = st.columns([1,2,1])
 
@@ -23,7 +23,7 @@ with col2.expander("więcej"):
     WAITER = st.number_input("poczekaj (po ilu dodatkowych dniach od spadku kupić)", value=5)
 
 #COLUMN 1
-col1.subheader("S&P500")
+col1.subheader("Infomacje")
 with col1.expander("Strategia oszczędzania"):
     st.write(f"Co **{BUY_SPACE} dni** odłóż **{AMOUNT} $** na zakup indeksu, poczekaj **{DAYS} dni** i ciesz się wynikami. W tym konkretnym modelu **dodatkowo** algorytm 'kupuje' indeks **{WAITER} dni** po tym jak **obecna cena jest niższa ze średniej ceny z {DAYS_AVG} dni**. Ten prosty sposób na kupowanie jest zastosowany przez bota, jednak wdrożenie go w życie nie jest czymś niemożliwym. Wystarczy co jakiś czas sprawdzać kurs indeksu i dokupować na lokalnych dołkach.")
 with col1.expander("Czym jest S&P500"):
